6e5ec6e8d7838bb7897908c3028ea4d6864e57f6
Peeyush <114051459+Peeyush-Pareek@users.noreply.github.com>
2023-11-11 09:12:06 +0530

    Update JavaExceptionEg.java
    JavaExceptionExample

--- JavaExceptionEg.java
+++ JavaExceptionEg.java
@@ -1,5 +1,10 @@
-try{
-      int data=100/0;
-   }catch(ArithmeticException e){System.out.println(e);}
-   System.out.println("rest of the code...");
-  }
+public class JavaExceptionEg{  
+  public static void main(String args[]){  
+   try{  
+      //code that may raise exception  
+      int data=200/0;  
+   }catch(ArithmeticException e){System.out.println(e);}  
+   //rest code of the program   
+   System.out.println("out of catch block");  
+  }  
+}  
