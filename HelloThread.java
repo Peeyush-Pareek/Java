d7bc8923eeaad79f610e0feb5d4e226dc4112e6c
Peeyush <114051459+Peeyush-Pareek@users.noreply.github.com>
2023-12-02 07:58:47 +0530

    Create HelloThread.java
    Basic Thread Creation

---
+++ HelloThread.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+      
+    public static void main(String[] args) {
+        // creating an object of the Thread class using the constructor Thread(String name)   
+Thread t= new Thread("My first thread");  
+  
+// the start() method moves the thread to the active state  
+t.start();  
+// getting the thread name by invoking the getName() method  
+String str = t.getName();  
+System.out.println(str);   
+    }
+}
